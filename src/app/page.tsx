e4fe35673f3faa851b3a309c5ec54330e7505863
Sam Selikoff <sam.selikoff@gmail.com>
2024-05-04 10:52:12 -0400

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,180 @@
-import ResizablePanel from "./resizable-panel";
+"use client";
+import { ComponentProps, useState } from "react";
+import * as ResizablePanel from "./resizable-panel";
+import { ArrowPathIcon } from "@heroicons/react/16/solid";
 
-export default function Home() {
-  return <ResizablePanel />;
+export default function Page() {
+  let [active, setActive] = useState("a");
+  let [isSending, setIsSending] = useState(false);
+
+  return (
+    <div className="relative">
+      <div className="absolute right-4 top-4">
+        <button
+          className="text-gray-600 transition hover:text-gray-300"
+          onClick={() => setActive("a")}
+        >
+          <ArrowPathIcon className="size-5" />
+        </button>
+      </div>
+
+      <div className="px-4">
+        <div className="mx-auto max-w-md pt-16 sm:pt-12 md:pt-20">
+          <div className="rounded-lg bg-gray-700 shadow-md shadow-black/30">
+            <ResizablePanel.Root active={active} className="px-4 py-8 md:p-8">
+              <ResizablePanel.Content value="a">
+                <p className="font-semibold text-white md:text-xl">
+                  Reset password
+                </p>
+                <p className="mt-2 text-xs text-gray-300 md:text-sm">
+                  Enter your email to get a password reset link.
+                </p>
+                <label className="mt-8 block text-sm font-medium text-white">
+                  Email address
+                  <input
+                    type="text"
+                    defaultValue="sam@buildui.com"
+                    className="focus-visible:border-brand focus-visible:outline-brand mt-2 w-full rounded-lg border border-gray-600 bg-gray-800 px-3 py-2 focus-visible:outline"
+                  />
+                </label>
+                <div className="mt-6 text-right">
+                  <button
+                    onClick={async () => {
+                      setIsSending(true);
+                      await new Promise((resolve) => setTimeout(resolve, 1000));
+                      setActive("b");
+                      setIsSending(false);
+                    }}
+                    disabled={isSending}
+                    className="bg-brand hover:bg-brand-light rounded-md px-3 py-2 text-xs font-semibold text-white disabled:pointer-events-none md:text-sm"
+                  >
+                    <Spinner isLoading={isSending}>Reset your password</Spinner>
+                  </button>
+                </div>
+              </ResizablePanel.Content>
+              <ResizablePanel.Content value="b">
+                <p className="font-semibold text-white md:text-xl">
+                  Email sent!
+                </p>
+                <p className="mt-2 text-xs text-gray-300 md:text-sm">
+                  Check your inbox to continue.
+                </p>
+              </ResizablePanel.Content>
+            </ResizablePanel.Root>
+          </div>
+          <div className="mt-6">
+            <p className="text-xs text-gray-500 md:text-sm">
+              <span className="underline underline-offset-2">Reach out</span> to
+              us if you need more help.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Spinner({
+  isLoading,
+  children,
+  ...rest
+}: { isLoading: boolean } & ComponentProps<"svg">) {
+  return (
+    <span className="relative">
+      {isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center">
+          <svg
+            viewBox="0 0 24 24"
+            className="max-h-full animate-spin"
+            style={{
+              animationTimingFunction: "steps(8, end)",
+              animationDuration: ".75s",
+            }}
+            {...rest}
+          >
+            <rect
+              style={{ opacity: 0.4 }}
+              x={11}
+              y={2}
+              width={2}
+              height={6}
+              rx={1}
+              fill="currentColor"
+            />
+            <rect
+              style={{ opacity: 0.4 }}
+              x={18.364}
+              y={4.22183}
+              width={2}
+              height={6}
+              rx={1}
+              transform="rotate(45 18.364 4.222)"
+              fill="currentColor"
+            />
+            <rect
+              x={22}
+              y={11}
+              width={2}
+              style={{ opacity: 0.4 }}
+              height={6}
+              rx={1}
+              transform="rotate(90 22 11)"
+              fill="currentColor"
+            />
+            <rect
+              x={19.7782}
+              y={18.364}
+              width={2}
+              style={{ opacity: 0.4 }}
+              height={6}
+              rx={1}
+              transform="rotate(135 19.778 18.364)"
+              fill="currentColor"
+            />
+            <rect
+              x={13}
+              y={22}
+              width={2}
+              style={{ opacity: 0.4 }}
+              height={6}
+              rx={1}
+              transform="rotate(-180 13 22)"
+              fill="currentColor"
+            />
+            <rect
+              x={5.63603}
+              y={19.7782}
+              width={2}
+              style={{ opacity: 0.6 }}
+              height={6}
+              rx={1}
+              transform="rotate(-135 5.636 19.778)"
+              fill="currentColor"
+            />
+            <rect
+              x={2}
+              y={13}
+              width={2}
+              style={{ opacity: 0.8 }}
+              height={6}
+              rx={1}
+              transform="rotate(-90 2 13)"
+              fill="currentColor"
+            />
+            <rect
+              x={4.22183}
+              y={5.63603}
+              width={2}
+              height={6}
+              rx={1}
+              transform="rotate(-45 4.222 5.636)"
+              fill="currentColor"
+            />
+          </svg>
+        </div>
+      )}
+
+      <span className={isLoading ? "invisible" : ""}>{children}</span>
+    </span>
+  );
 }
--- src/app/resizable-panel.tsx
+++ src/app/resizable-panel.tsx
@@ -1,19 +1,12 @@
 "use client";
 
-import { ArrowPathIcon } from "@heroicons/react/16/solid";
 import { AnimatePresence, motion } from "framer-motion";
-import {
-  ComponentProps,
-  ReactNode,
-  createContext,
-  useContext,
-  useState,
-} from "react";
+import { ComponentProps, ReactNode, createContext, useContext } from "react";
 import useMeasure from "react-use-measure";
 
 let PanelContext = createContext({ active: "" });
 
-function PanelRoot({
+export function Root({
   children,
   active,
   ...rest
@@ -38,7 +31,7 @@ function PanelRoot({
   );
 }
 
-function PanelContent({
+export function Content({
   value,
   children,
   ...rest
@@ -76,181 +69,5 @@ function PanelContent({
         </motion.div>
       )}
     </AnimatePresence>
-  );
-}
-
-export default function ResizablePanel() {
-  let [active, setActive] = useState("a");
-  let [isSending, setIsSending] = useState(false);
-
-  return (
-    <div className="relative">
-      <div className="absolute right-4 top-4">
-        <button
-          className="text-gray-600 transition hover:text-gray-300"
-          onClick={() => setActive("a")}
-        >
-          <ArrowPathIcon className="size-5" />
-        </button>
-      </div>
-
-      <div className="px-4">
-        <div className="mx-auto max-w-md pt-16 sm:pt-12 md:pt-20">
-          <div className="rounded-lg bg-gray-700 shadow-md shadow-black/30">
-            <PanelRoot active={active} className="px-4 py-8 md:p-8">
-              <PanelContent value="a">
-                <p className="font-semibold text-white md:text-xl">
-                  Reset password
-                </p>
-                <p className="mt-2 text-xs text-gray-300 md:text-sm">
-                  Enter your email to get a password reset link.
-                </p>
-                <label className="mt-8 block text-sm font-medium text-white">
-                  Email address
-                  <input
-                    type="text"
-                    defaultValue="sam@buildui.com"
-                    className="focus-visible:border-brand focus-visible:outline-brand mt-2 w-full rounded-lg border border-gray-600 bg-gray-800 px-3 py-2 focus-visible:outline"
-                  />
-                </label>
-                <div className="mt-6 text-right">
-                  <button
-                    onClick={async () => {
-                      setIsSending(true);
-                      await new Promise((resolve) => setTimeout(resolve, 1000));
-                      setActive("b");
-                      setIsSending(false);
-                    }}
-                    disabled={isSending}
-                    className="bg-brand hover:bg-brand-light rounded-md px-3 py-2 text-xs font-semibold text-white disabled:pointer-events-none md:text-sm"
-                  >
-                    <Spinner isLoading={isSending}>Reset your password</Spinner>
-                  </button>
-                </div>
-              </PanelContent>
-              <PanelContent value="b">
-                <p className="font-semibold text-white md:text-xl">
-                  Email sent!
-                </p>
-                <p className="mt-2 text-xs text-gray-300 md:text-sm">
-                  Check your inbox to continue.
-                </p>
-              </PanelContent>
-            </PanelRoot>
-          </div>
-          <div className="mt-6">
-            <p className="text-xs text-gray-500 md:text-sm">
-              <span className="underline">Reach out</span> to us if you need
-              more help.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Spinner({
-  isLoading,
-  children,
-  ...rest
-}: { isLoading: boolean } & ComponentProps<"svg">) {
-  return (
-    <span className="relative">
-      {isLoading && (
-        <div className="absolute inset-0 flex items-center justify-center">
-          <svg
-            viewBox="0 0 24 24"
-            className="max-h-full animate-spin"
-            style={{
-              animationTimingFunction: "steps(8, end)",
-              animationDuration: ".75s",
-            }}
-            {...rest}
-          >
-            <rect
-              style={{ opacity: 0.4 }}
-              x={11}
-              y={2}
-              width={2}
-              height={6}
-              rx={1}
-              fill="currentColor"
-            />
-            <rect
-              style={{ opacity: 0.4 }}
-              x={18.364}
-              y={4.22183}
-              width={2}
-              height={6}
-              rx={1}
-              transform="rotate(45 18.364 4.222)"
-              fill="currentColor"
-            />
-            <rect
-              x={22}
-              y={11}
-              width={2}
-              style={{ opacity: 0.4 }}
-              height={6}
-              rx={1}
-              transform="rotate(90 22 11)"
-              fill="currentColor"
-            />
-            <rect
-              x={19.7782}
-              y={18.364}
-              width={2}
-              style={{ opacity: 0.4 }}
-              height={6}
-              rx={1}
-              transform="rotate(135 19.778 18.364)"
-              fill="currentColor"
-            />
-            <rect
-              x={13}
-              y={22}
-              width={2}
-              style={{ opacity: 0.4 }}
-              height={6}
-              rx={1}
-              transform="rotate(-180 13 22)"
-              fill="currentColor"
-            />
-            <rect
-              x={5.63603}
-              y={19.7782}
-              width={2}
-              style={{ opacity: 0.6 }}
-              height={6}
-              rx={1}
-              transform="rotate(-135 5.636 19.778)"
-              fill="currentColor"
-            />
-            <rect
-              x={2}
-              y={13}
-              width={2}
-              style={{ opacity: 0.8 }}
-              height={6}
-              rx={1}
-              transform="rotate(-90 2 13)"
-              fill="currentColor"
-            />
-            <rect
-              x={4.22183}
-              y={5.63603}
-              width={2}
-              height={6}
-              rx={1}
-              transform="rotate(-45 4.222 5.636)"
-              fill="currentColor"
-            />
-          </svg>
-        </div>
-      )}
-
-      <span className={isLoading ? "invisible" : ""}>{children}</span>
-    </span>
   );
 }
